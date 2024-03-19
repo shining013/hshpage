e35ad43236337ba3b42277692271561b3e568a43
shining013 <nevertheless113@naver.com>
2024-03-19 18:21:55 +0900

    Initial commit

---
+++ src/main/java/hsh/hshpage/HshpageApplication.java
@@ -0,0 +1,13 @@
+package hsh.hshpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HshpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HshpageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hsh/hshpage/HshpageApplicationTests.java
@@ -0,0 +1,13 @@
+package hsh.hshpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HshpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
